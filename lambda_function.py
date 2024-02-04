1ecb84793546d9b8995013cb221bd41e08cce065
CharlieAram <charlie21aram@gmail.com>
2024-02-04 02:21:10 +0000

    update ready for lambda upload

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,8 @@
-from predict import predict
+from backend.model.predict import predict
 import backend.routing.graph_hopper_manager as graph_hopper_manager
 import json
 
-def main(dept, dest):
+def lambda_handler(dept, dest):
 
     try:
         route = graph_hopper_manager.get_route(dept=dept, dest=dest)
@@ -18,3 +18,6 @@ def main(dept, dest):
 
 # with open('new.json', 'w') as f:
 #     json.dump(main([51.515273, -0.132233], [51.511425, -0.120112]), f)
+    
+
+print(lambda_handler([51.515273, -0.132233], [51.511425, -0.120112]))
