3cb617e1a059a2e0cda77aafd02ace282308e2be
zhenglong-wu <wu.zhenglong@outlook.com>
2024-02-04 01:57:51 +0000

    LESGOO

---
+++ backend/model/main.py
@@ -0,0 +1,15 @@
+from predict import predict
+import backend.routing.graph_hopper_manager as graph_hopper_manager
+import json
+
+def main(dept, dest):
+
+    route = graph_hopper_manager.get_route(dept=dept, dest=dest)
+    scores = [predict(long=x[0], lat=x[1]) for x in route['data']['paths'][0]['points']['coordinates']]
+    route['safety_score'] = sum(scores)/len(scores)
+
+    return json.dumps(route)
+
+
+# with open('new.json', 'w') as f:
+#     json.dump(main([51.515273, -0.132233], [51.511425, -0.120112]), f)
